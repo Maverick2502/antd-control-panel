529507c1406f7cb5faa83e1236b454d4e4129aaf
mkuysunov <mkuysunov@gmail.com>
2022-03-09 14:43:10 +0500

    add RU language

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import { BrowserRouter } from "react-router-dom";
+import { ConfigProvider } from "antd";
+import ru_RU from "antd/lib/locale/ru_RU";
 
 // initialization axios baseUrl
 import "./utils";
@@ -14,9 +16,11 @@ import "./fonts/inter.css";
 
 ReactDOM.render(
   <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ConfigProvider locale={ru_RU}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ConfigProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
