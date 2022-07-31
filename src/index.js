b52850f51a82a48652c8def7132bc6a0e4c4932e
Nurullo <khuwaidullaev@gmail.com>
2022-07-31 16:29:12 +0500

    Changed index.js configuration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { Provider } from "react-redux";
-import { store } from "./store";
-import { BrowserRouter } from "react-router-dom";
 import { ConfigProvider } from "antd";
 import ru_RU from "antd/lib/locale/ru_RU";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { store } from "./store";
 
 // initialization axios baseUrl
 import "./utils";
@@ -14,13 +14,14 @@ import "./utils";
 import "./antd.config.less";
 import "./fonts/inter.css";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <BrowserRouter>
     <ConfigProvider locale={ru_RU}>
       <Provider store={store}>
         <App />
       </Provider>
     </ConfigProvider>
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
